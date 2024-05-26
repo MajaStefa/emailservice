76088d680f7e50e3c91ac243b9b071d91f45d12e
MajaStefa <marija.stefanovic004@gmail.com>
2024-05-26 10:32:45 +0200

    inital backend

---
+++ emailserviceback/src/main/java/com/emailservice/emailserviceback/EmailservicebackApplication.java
@@ -0,0 +1,13 @@
+package com.emailservice.emailserviceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailservicebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailservicebackApplication.class, args);
+	}
+
+}
---
+++ emailserviceback/src/test/java/com/emailservice/emailserviceback/EmailservicebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailservice.emailserviceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailservicebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
